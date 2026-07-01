47449c0576f8303b886ec53b87d48b758cbac1a0
Eiddy <eiddy135@gmail.com>
2026-07-02 00:53:43 +0800

    chore: add net watcher plugin scaffold

---
+++ tauri-plugin-net-watcher/src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<NetWatcher<R>> {
+  Ok(NetWatcher(app.clone()))
+}
+
+/// Access to the net-watcher APIs.
+pub struct NetWatcher<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> NetWatcher<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
---
+++ tauri-plugin-net-watcher/src/lib.rs
@@ -0,0 +1,48 @@
+use tauri::{
+  plugin::{Builder, TauriPlugin},
+  Manager, Runtime,
+};
+
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
+mod error;
+mod models;
+
+pub use error::{Error, Result};
+
+#[cfg(desktop)]
+use desktop::NetWatcher;
+#[cfg(mobile)]
+use mobile::NetWatcher;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the net-watcher APIs.
+pub trait NetWatcherExt<R: Runtime> {
+  fn net_watcher(&self) -> &NetWatcher<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::NetWatcherExt<R> for T {
+  fn net_watcher(&self) -> &NetWatcher<R> {
+    self.state::<NetWatcher<R>>().inner()
+  }
+}
+
+/// Initializes the plugin.
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("net-watcher")
+    .invoke_handler(tauri::generate_handler![commands::ping])
+    .setup(|app, api| {
+      #[cfg(mobile)]
+      let net_watcher = mobile::init(app, api)?;
+      #[cfg(desktop)]
+      let net_watcher = desktop::init(app, api)?;
+      app.manage(net_watcher);
+      Ok(())
+    })
+    .build()
+}
